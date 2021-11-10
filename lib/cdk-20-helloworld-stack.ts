3750f2aed041bb7c81c43d0df8f98b16d2fcee28
EC2 Default User <ec2-user@ip-172-31-28-208.us-west-1.compute.internal>
2021-11-10 22:17:53 +0000

    first commit

--- lib/cdk-20-helloworld-stack.ts
+++ lib/cdk-20-helloworld-stack.ts
@@ -1,16 +1,31 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class Cdk20HelloworldStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'Cdk20HelloworldQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,    
+      code: new lambda.InlineCode(`
+        exports.handler = async function(event) {
+          console.log("request:", JSON.stringify(event, undefined, 2));
+          return {
+            statusCode: 200,
+            headers: { "Content-Type": "text/plain" },
+            body: "Hello, CDK!!! You've hit " + event.path
+          };
+        };
+        `),
+      handler: 'index.handler'
+    });
+    
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
+    
   }
 }
